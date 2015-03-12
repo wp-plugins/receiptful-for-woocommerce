db05ae829cb9bb4864de3bb4a1bff0a1ec00e252
receiptful <receiptful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 13:16:44 +0000

    Tagging Receiptful for WooCommerce 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/receiptful-for-woocommerce/tags/1.1.2@1111093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/receiptful-cron-functions.php
+++ includes/receiptful-cron-functions.php
@@ -1,13 +1,13 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
 /**
  * CRON events.
  *
  * @author		Receiptful
- * @version		1.0.1
+ * @version		1.1.1
  * @since		1.0.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 
 /**
@@ -17,20 +17,20 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *
  * @since 1.0.0
  *
- * @param 	array $schedules	List of current CRON schedules.
- * @return 	array				List of modified CRON schedules.
+ * @param	array $schedules	List of current CRON schedules.
+ * @return	array				List of modified CRON schedules.
  */
-add_filter( 'cron_schedules', 'receiptful_add_quarter_schedule' );
 function receiptful_add_quarter_schedule( $schedules ) {
 
 	$schedules['quarter_hour'] = array(
-		'interval' 	=> 60 * 15, // 60 seconds * 15 minutes
-		'display' 	=> __( 'Every quarter', 'receiptful' )
+		'interval'	=> 60 * 15, // 60 seconds * 15 minutes
+		'display'	=> __( 'Every quarter', 'receiptful' ),
 	);
 
 	return $schedules;
 
 }
+add_filter( 'cron_schedules', 'receiptful_add_quarter_schedule' );
 
 
 /**
@@ -42,40 +42,211 @@ function receiptful_add_quarter_schedule( $schedules ) {
  *
  * @since 1.0.0
  */
-// Schedule resend receipts event
-if ( ! wp_next_scheduled( 'receiptful_check_resend' ) ) {
-	wp_schedule_event( 1407110400, 'quarter_hour', 'receiptful_check_resend' ); // 1407110400 is 08 / 4 / 2014 @ 0:0:0 UTC
+function receiptful_schedule_event() {
+
+	// Resend queue
+	if ( ! wp_next_scheduled( 'receiptful_check_resend' ) ) {
+		wp_schedule_event( 1407110400, 'quarter_hour', 'receiptful_check_resend' ); // 1407110400 is 08 / 4 / 2014 @ 0:0:0 UTC
+	}
+
+	// Initial product sync
+	if ( ! wp_next_scheduled( 'receiptful_initial_product_sync' ) && 1 != get_option( 'receiptful_completed_initial_product_sync', 0 ) ) {
+		wp_schedule_event( 1407110400, 'quarter_hour', 'receiptful_initial_product_sync' ); // 1407110400 is 08 / 4 / 2014 @ 0:0:0 UTC
+	} elseif ( wp_next_scheduled( 'receiptful_initial_product_sync' ) && 1 == get_option( 'receiptful_completed_initial_product_sync', 0 ) ) {
+		// Remove CRON when we're done with it.
+		wp_clear_scheduled_hook( 'receiptful_initial_product_sync' );
+	}
+
+	// Initial receipt sync
+	if ( ! wp_next_scheduled( 'receiptful_initial_receipt_sync' ) && 1 != get_option( 'receiptful_completed_initial_receipt_sync', 0 ) ) {
+		wp_schedule_event( 1407110400, 'quarter_hour', 'receiptful_initial_receipt_sync' ); // 1407110400 is 08 / 4 / 2014 @ 0:0:0 UTC
+	} elseif ( wp_next_scheduled( 'receiptful_initial_receipt_sync' ) && 1 == get_option( 'receiptful_completed_initial_receipt_sync', 0 ) ) {
+		wp_clear_scheduled_hook( 'receiptful_initial_receipt_sync' ); // Remove CRON when we're done with it.
+	}
+
 }
+add_action( 'init', 'receiptful_schedule_event' );
 
 
 /**
- * Cron function.
+ * Resend queue.
  *
- * Function being fired by CRON. Resend any receipts in queue.
+ * Function is called every 15 minutes by a CRON job.
+ * This fires the resend of Receipts and data that should be synced.
  *
  * @since 1.0.0
  */
-add_action( 'receiptful_check_resend', 'receiptful_check_resend' );
 function receiptful_check_resend() {
 
+	// Receipt queue
 	Receiptful()->email->resend_queue();
 
+	// Products queue
+	Receiptful()->products->process_queue();
+
 }
+add_action( 'receiptful_check_resend', 'receiptful_check_resend' );
+
 
 /**
- * Do not copy receiptful meta data for WC Subscription renewals
+ * Sync data.
  *
- * @param $order_meta_query
- * @param $original_order_id
- * @param $renewal_order_id
- * @param $new_order_role
+ * Sync data with the Receiptful API, this contains products for now.
+ * The products are synced with Receiptful to give the best product recommendations.
+ * This is a initial product sync, the process should be completed once.
  *
- * @return string
+ * @since 1.1.1
  */
-function receiptful_do_not_copy_meta_data( $order_meta_query, $original_order_id, $renewal_order_id, $new_order_role ) {
+function receiptful_initial_product_sync() {
 
-	$order_meta_query .= " AND `meta_key` NOT IN ('_receiptful_receipt_id', '_receiptful_web_link')";
+	$product_ids = get_posts( array(
+		'fields'			=> 'ids',
+		'posts_per_page'	=> '225',
+		'post_type'			=> 'product',
+		'meta_query'		=> array(
+			array(
+				'key'		=> '_receiptful_last_update',
+				'compare'	=> 'NOT EXISTS',
+				'value'		=> '',
+			),
+		),
+	) );
 
-	return $order_meta_query;
+	// Update option so the system knows it should stop syncing
+	if ( empty( $product_ids ) ) {
+		update_option( 'receiptful_completed_initial_product_sync', 1 );
+		return;
+	}
+
+	// Get product args
+	$args = array();
+	foreach ( $product_ids as $product_id ) {
+		$args[] = Receiptful()->products->get_formatted_product( $product_id );
+	}
+
+	// Update products
+	$response = Receiptful()->api->update_products( $args );
+
+	// Process response
+	if ( is_wp_error( $response ) ) {
+
+		return false;
+
+	} elseif ( in_array( $response['response']['code'], array( '400' ) ) ) {
+
+		// Set empty update time, so its not retried at next CRON job
+		foreach ( $product_ids as $product_id ) {
+			update_post_meta( $product_id, '_receiptful_last_update', '' );
+		}
+
+	} elseif ( in_array( $response['response']['code'], array( '200', '202' ) ) ) { // Update only the ones without error - retry the ones with error
+
+		$failed_ids = array();
+		$body 		= json_decode( $response['body'], 1 );
+		foreach ( $body['errors'] as $error ) {
+			$failed_ids[] = $error['error']['product_id'];
+		}
+
+		// Set empty update time, so its not retried at next CRON job
+		foreach ( $product_ids as $product_id ) {
+			if ( ! in_array( $product_id, $failed_ids ) ) {
+				update_post_meta( $product_id, '_receiptful_last_update', time() );
+			} else {
+				update_post_meta( $product_id, '_receiptful_last_update', '' );
+			}
+		}
+
+	} elseif ( in_array( $response['response']['code'], array( '401', '500', '503' ) ) ) { // Retry later - keep meta unset
+	}
+
 }
-add_filter( 'woocommerce_subscriptions_renewal_order_meta_query', 'receiptful_do_not_copy_meta_data', 10, 4 );
+add_action( 'receiptful_initial_product_sync', 'receiptful_initial_product_sync' );
+
+
+/**
+ * Sync Receipt data.
+ *
+ * Sync data with the Receiptful API, this contains products for now.
+ * The products are synced with Receiptful to give the best product recommendations.
+ * This is a initial product sync, the process should be completed once.
+ *
+ * @since 1.1.2
+ */
+function receiptful_initial_receipt_sync() {
+
+	$receipt_ids = get_posts( array(
+		'fields'			=> 'ids',
+		'posts_per_page'	=> '225',
+		'post_type'			=> 'shop_order',
+		'post_status'		=> array_keys( wc_get_order_statuses() ),
+		'meta_query'		=> array(
+			array(
+				'key'		=> '_receiptful_last_update',
+				'compare'	=> 'NOT EXISTS',
+				'value'		=> '',
+			),
+			array(
+				'key'		=> '_receiptful_receipt_id',
+				'compare'	=> 'NOT EXISTS',
+				'value'		=> '',
+			),
+		),
+	) );
+
+	// Update option so the system knows it should stop syncing
+	if ( empty( $receipt_ids ) ) {
+		update_option( 'receiptful_completed_initial_receipt_sync', 1 );
+		return;
+	}
+
+	// Prepare product args
+	$args = array();
+	foreach ( $receipt_ids as $receipt_id ) {
+
+		$order				= wc_get_order( $receipt_id );
+		$items 				= WC()->mailer->emails['WC_Email_Customer_Completed_Order']->api_args_get_items( $order );
+		$subtotals 			= WC()->mailer->emails['WC_Email_Customer_Completed_Order']->api_args_get_subtotals( $order );
+		$related_products 	= WC()->mailer->emails['WC_Email_Customer_Completed_Order']->api_args_get_related_products( $items );
+		$order_args 		= WC()->mailer->emails['WC_Email_Customer_Completed_Order']->api_args_get_order_args( $order, $items, $subtotals, $related_products );
+
+		$args[] = $order_args;
+
+	}
+
+	// Update products
+	$response = Receiptful()->api->upload_receipts( $args );
+
+	// Process response
+	if ( is_wp_error( $response ) ) {
+
+		return false;
+
+	} elseif ( in_array( $response['response']['code'], array( '400' ) ) ) {
+
+		// Set empty update time, so its not retried at next CRON job
+		foreach ( $receipt_ids as $receipt_id ) {
+			update_post_meta( $receipt_id, '_receiptful_last_update', '' );
+		}
+
+	} elseif ( in_array( $response['response']['code'], array( '200', '202' ) ) ) { // Update only the ones without error - retry the ones with error
+
+		$failed_ids = array();
+		$body 		= json_decode( $response['body'], 1 );
+		foreach ( $body['errors'] as $error ) {
+			$failed_ids[] = isset( $error['error']['product_id'] ) ? $error['error']['product_id'] : null;
+		}
+
+		// Set empty update time, so its not retried at next CRON job
+		foreach ( $receipt_ids as $receipt_id ) {
+			if ( ! in_array( $receipt_id, $failed_ids ) ) {
+				update_post_meta( $receipt_id, '_receiptful_last_update', time() );
+			} else {
+				update_post_meta( $receipt_id, '_receiptful_last_update', '' );
+			}
+		}
+
+	} elseif ( in_array( $response['response']['code'], array( '401', '500', '503' ) ) ) { // Retry later - keep meta unset
+	}
+
+}
+add_action( 'receiptful_initial_receipt_sync', 'receiptful_initial_receipt_sync' );
